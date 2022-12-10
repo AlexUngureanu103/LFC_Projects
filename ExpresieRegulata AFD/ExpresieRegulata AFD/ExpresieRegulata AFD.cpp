1e4d2c97ec26fb71fcd6e83b44345dfb2553c304
AlexUnguranu <a.ungureanu0099@gmail.com>
2022-12-10 16:27:36 +0200

    Added initial solution for the 2 homeworks
    Forma poloneza
    Push-Down

---
+++ ExpresieRegulata AFD/ExpresieRegulata AFD/ExpresieRegulata AFD.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+// Expresie regulata -> AFD
+// #forma poloneza 
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ ExpresieRegulata AFD/GramIDC Push-Down/GramIDC Push-Down.cpp
@@ -0,0 +1,9 @@
+
+
+#include <iostream>
+ // Tema cu PUSH DOWN 
+// sau alegem interfata grafica pentru un automat
+int main()
+{
+    std::cout << "Hello World!\n";
+}
