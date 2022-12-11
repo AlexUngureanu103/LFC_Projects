45982a7d077a6ee88e7c4a1ecfc13667370c629d
AlexUnguranu <a.ungureanu0099@gmail.com>
2022-12-11 17:04:33 +0200

    Am adaugat getters pentru noua clasa de automat

--- LFC_Teme_Echipa/ExpresieRegulata AFD/AutomatLambdaTranzitii.cpp
+++ LFC_Teme_Echipa/ExpresieRegulata AFD/AutomatLambdaTranzitii.cpp
@@ -15,14 +15,30 @@ void AutomatLambdaTranzitii::setQ(const std::vector<std::string>& State)
 	this->m_State = State;
 }
 
+std::vector<std::string> AutomatLambdaTranzitii::getQ() const
+{
+	return this->m_State;
+}
+
 void AutomatLambdaTranzitii::setEntryAlphabet(const std::vector<char>& alphabet)
 {
 	this->m_entryAlphabet = alphabet;
 }
 
+std::vector<char> AutomatLambdaTranzitii::getEntryAlphabet() const
+{
+	return this->m_entryAlphabet;
+}
+
+
 void AutomatLambdaTranzitii::setInitialState(const std::string& state)
 {
 	this->m_initialState = state;
+}
+
+std::string AutomatLambdaTranzitii::getInitialState() const
+{
+	return this->m_initialState;
 }
 
 void AutomatLambdaTranzitii::setFinState(const std::string& state)
@@ -30,13 +46,23 @@ void AutomatLambdaTranzitii::setFinState(const std::string& state)
 	this->m_FinState = state;
 }
 
+std::string AutomatLambdaTranzitii::getFinState() const
+{
+	return this->m_FinState;
+}
+
 void AutomatLambdaTranzitii::setTransition(const std::vector<std::tuple<std::string, char, std::string>>& transition)
 {
 	this->m_transition = transition;
 }
 
+std::vector<AutomatLambdaTranzitii::Transition> AutomatLambdaTranzitii::getTransition() const
+{
+	return this->m_transition;
+}
+
 AutomatLambdaTranzitii& ConcatenateAutomates(AutomatLambdaTranzitii& firstAutomate, AutomatLambdaTranzitii& secondAutomate)
 {
 	AutomatLambdaTranzitii newAutomatLambdaTransiti;
-
+	std::string newInitialState = firstAutomate.
 }
--- LFC_Teme_Echipa/ExpresieRegulata AFD/AutomatLambdaTranzitii.h
+++ LFC_Teme_Echipa/ExpresieRegulata AFD/AutomatLambdaTranzitii.h
@@ -11,14 +11,20 @@ class AutomatLambdaTranzitii
 	std::vector<std::tuple<std::string, char, std::string>> m_transition;
 
 public:
+	using Transition = std::tuple<std::string, char, std::string>;
 	AutomatLambdaTranzitii();
 	AutomatLambdaTranzitii(const std::string& initialState, const char& entryAlphabet, const std::string& finalState);
 	void setQ(const std::vector<std::string>& State);
+	std::vector<std::string> getQ() const;
 	void setEntryAlphabet(const std::vector<char>& alphabet);
+	std::vector<char> getEntryAlphabet() const;
 	void setInitialState(const std::string& state);
+	std::string getInitialState() const;
 	void setFinState(const std::string& state);
+	std::string getFinState() const;
 	void setTransition(const std::vector<std::tuple<std::string, char, std::string>>& transition);
-	
+	std::vector<Transition> getTransition() const;
+
 };
 
 AutomatLambdaTranzitii& ConcatenateAutomates(AutomatLambdaTranzitii& firstAutomate, AutomatLambdaTranzitii& secondAutomate);
